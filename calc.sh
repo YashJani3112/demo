deefe1c7e7d5b2ca486d7adbe450d8e2208c269e
YashJani3112 <yashjani30201@gmail.com>
2025-07-20 19:58:21 +0530

    Second Commit

--- calc.sh
+++ calc.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 
+echo "Choose operation:"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read -p "Enter your choice (1/2/3/4): " choice
 
-echo "Calculator"
+SCALE="scale=2;"
+read -p "Enter First Input: " num1
+read -p "Enter Second Input: " num2
+
+case $choice in
+
+	1)
+		echo "Sum = $(echo "$SCALE $num1+$num2" |bc)"
+		
+		;;
+	
+	2)
+		#sub=$(echo "$num1-$num2" |bc)
+		  echo "Diff = $(echo "$SCALE $num1-$num2" |bc)"
+
+                ;;
+
+	3)
+		#mul=$(echo "$num1*$num2" |bc)
+		  echo "Product = $(echo "$SCALE $num1*$num2" |bc)"
+
+                ;;
+
+	4) 
+		echo "Division = $(echo "$SCALE  $num1/$num2" |bc)"
+
+		;;
+
+
+	*)
+		echo "Invalid "
+
+		;;
+
+esac
+
+
+
