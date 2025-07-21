f37742d63541a3ac82ca73d80925132d28f1638a
YashJani3112 <yashjani30201@gmail.com>
2025-07-21 15:53:30 +0530

    v1

---
+++ date.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+date=$(date +"%d-%m-%y")
+
+time=$(date +"%H:%M:%S")
+time2=$(date +"%r")
+
+echo "Date: $date"
+echo "Time in 24hr format: $time"
+echo "Time in 12hs format: $time2"
